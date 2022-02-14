3b91a65f8958ceb6eae7ad4a7cb41feb3e3c115c
wubianluoye <575784835@qq.com>
2022-02-14 17:47:23 +0800

    add loading

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,23 +1,42 @@
-import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse, AxiosPromise, AxiosError }  from 'axios'
+import axios, {
+  AxiosRequestConfig,
+  AxiosInstance,
+  AxiosResponse,
+  AxiosPromise,
+  AxiosError,
+} from "axios";
+import { Toast } from "vant";
 
 const http = axios.create({
-  baseURL: '',
-  timeout: 6000
-}) as AxiosInstance
+  baseURL: "",
+  timeout: 6000,
+}) as AxiosInstance;
 
-http.interceptors.request.use((config: AxiosRequestConfig): AxiosRequestConfig => {
-  return config
-}, (error: AxiosError) => {
-  return Promise.reject(error)
-})
-
-http.interceptors.response.use((response: AxiosResponse): AxiosPromise => {
-  if(response.status!==200 || response.data.code!==200) {
-    return Promise.reject(response.data)
+http.interceptors.request.use(
+  (config: AxiosRequestConfig): AxiosRequestConfig => {
+    Toast.loading({
+      message: "加载中...",
+      forbidClick: true,
+      loadingType: "spinner",
+    });
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
   }
-  return Promise.resolve(response.data)
-}, error => {
-  return Promise.reject(error)
-})
+);
 
-export default http
+http.interceptors.response.use(
+  (response: AxiosResponse): AxiosPromise => {
+    Toast.clear();
+    if (response.status !== 200 || response.data.code !== 200) {
+      return Promise.reject(response.data);
+    }
+    return Promise.resolve(response.data);
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default http;
