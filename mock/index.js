d2aeeed53264ec5830e44e271ca8227e024011c1
wubianluoye <575784835@qq.com>
2022-02-14 17:47:53 +0800

    update mock

--- mock/index.js
+++ mock/index.js
@@ -1,76 +1,126 @@
-import Mock from 'mockjs'
-const Random = Mock.Random
+import Mock from "mockjs";
+// Mock.setup({
+//   timeout: "10-600",
+// });
 
-function createList(min=0, max=10) {
-  const len = Random.integer(min, max)
-  const arr = []
-  for(let i=0;i<len;i++) {
+const Random = Mock.Random;
+
+function createList(min = 0, max = 10) {
+  const len = Random.integer(min, max);
+  const arr = [];
+  for (let i = 0; i < len; i++) {
     const obj = {
-      username: Mock.nickname(),
-      password: Random.id()
-    }
-    arr.push(obj)
+      id: Random.id(),
+      title: Random.cparagraph(20),
+      username: Random.name(),
+      password: Random.id(),
+    };
+    arr.push(obj);
   }
-  return arr
+  return arr;
 }
 
+function createOrder(min = 0, max = 10) {
+  const types = ["imp", "game", "gen"];
+  const len = Random.integer(min, max);
+  const arr = [];
+  for (let i = 0; i < len; i++) {
+    const obj = {
+      id: Random.increment(),
+      title: Random.cparagraph(1, 2),
+      name: Random.name(),
+      password: Random.id(),
+      type: Random.pick(types),
+      avat: Random.image('20x20', '#000000', '#fffff', 'jpg')
+    };
+    arr.push(obj);
+  }
+  return arr;
+}
 
 export default [
   {
-    url: '/login',
-    method: 'post',
-    response: res => {
+    url: "/login",
+    method: "post",
+    response: (res) => {
       const user = {
-        username: 'admin',
-        password: '123456'
-      }
-      const flag = Mock.valid(user, res.body)
-      if(!flag.length) {
+        username: "admin",
+        password: "123456",
+      };
+      const flag = Mock.valid(user, res.body);
+      if (!flag.length) {
         return {
           code: 200,
-          message: 'ok',
+          message: "ok",
           data: {
-            username: 'admin',
-            token: Random.guid()
-          }
-        }
-      }else{
+            username: "admin",
+            token: Random.guid(),
+          },
+        };
+      } else {
         return {
           code: 204,
-          message: '用户名或密码错误',
-          data: null
-        }
+          message: "用户名或密码错误",
+          data: null,
+        };
       }
-    }
+    },
   },
   {
-    url: '/home',
-    method: 'get',
-    response: res => {
-      const arr = createList()
-      return [
-        {
-          title: Mock.nickname(),
-          desc: Mock.text(),
-          create_at: Random.datetime('yyyy-MM-dd HH:mm:ss'),
-          total: arr.length,
-          list: arr
+    url: "/home",
+    method: "get",
+    response: (res) => {
+      const arr = createList();
+      return {
+        code: 200,
+        message: "ok",
+        data: [
+          {
+            title: Random.name(),
+            desc: Random.email(),
+            create_at: Random.datetime("yyyy-MM-dd HH:mm:ss"),
+            total: arr.length,
+            list: arr,
+          },
+          {
+            title: Random.name(),
+            desc: Random.email(),
+            create_at: Random.datetime("yyyy-MM-dd HH:mm:ss"),
+            total: arr.length,
+            list: arr,
+          },
+          {
+            title: Random.name(),
+            desc: Random.email(),
+            create_at: Random.datetime("yyyy-MM-dd HH:mm:ss"),
+            total: arr.length,
+            list: arr,
+          },
+        ],
+      };
+    },
+  },
+  {
+    url: "/list",
+    method: "get",
+    response: (res) => {
+      const type = res.url.split("?")[1].split("=")[1];
+      const list =
+        type === "all"
+          ? createOrder(20)
+          : createOrder(20).filter((item) => item.type === type);
+      const total = list.length;
+      const genTotal = list.filter((item) => item.type === "gen").length;
+
+      return {
+        code: 200,
+        message: "ok",
+        data: {
+          total,
+          gen_total: genTotal,
+          list,
         },
-        {
-          title: Mock.nickname(),
-          desc: Mock.text(),
-          create_at: Random.datetime('yyyy-MM-dd HH:mm:ss'),
-          total: arr.length,
-          list: arr
-        },
-        {
-          title: Mock.nickname(),
-          desc: Mock.text(),
-          create_at: Random.datetime('yyyy-MM-dd HH:mm:ss'),
-          total: arr.length,
-          list: arr
-        },
-      ]
-    }
-  }
-]
+      };
+    },
+  },
+];
