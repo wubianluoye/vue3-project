9e1dee2ae005ee84e66a08626717964aae2a7cdf
wubianluoye <575784835@qq.com>
2022-02-15 15:26:38 +0800

    store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import store from '@/store/index'
 import '@/styles/index.scss'
+
 
 import '../mock'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
